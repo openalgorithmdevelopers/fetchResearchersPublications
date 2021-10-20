b1d7c081c23f7beaedcc354752b34beb6b0312b0
Bhupendra Singh <bhupendrabisht59@gmail.com>
2021-10-20 17:26:02 +0530

    provided the number of next click available, fetches all the data records and generates report succesfully.

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ Created on Mon Oct 18 11:11:50 2021
 
 @author: bhupendra.singh
 """
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 18 13:12:45 2021
-
-@author: bhupendra.singh
-"""
 
 import os
 from selenium import webdriver
@@ -22,7 +16,8 @@ import pandas as pd
 
 
 os.chdir('C:/Users/bhupendra.singh/Documents/GitHub/fetchResearchersPublications')
-authorId = 57202256028
+authorId = 57221459727
+#authorId = 57210953023  #bhupendra singh
 url = "https://www.scopus.com/authid/detail.uri?authorId=" + str(authorId)
 
 
@@ -37,37 +32,52 @@ driver.refresh()
 time.sleep(15)
 print("title = " + driver.title)
 
-articleAndJournalNameWebElementList = driver.find_elements(By.XPATH, '//a[@title="Show document details"]') #Article and journal name
-articleTypeWebElementList = driver.find_elements(By.XPATH,  '//div[@data-component="document-type"]') #article or + open, conference paper
-
-articleInformationWebElementList = driver.find_elements(By.XPATH,  '//span[@class="text-meta"]') #year, vol, issue, page nos, article no/
-citationsCountWebElementList = driver.find_elements(By.XPATH,  '//div[@class="sc-els-info-field"]') #Citations count
-
-#players = driver.find_elements(By.XPATH,  '//div[@data-component="document-authors"]') #article or + open, conference paper
-authorsWebElementList = driver.find_elements(By.XPATH,  '//div[@class="author-list"]') #Authors lists
-
-
-print(len(articleTypeWebElementList))
-
 
 dataRows = []
 singleRow = []
 
-for i in range(len(articleTypeWebElementList)):
-    print(articleTypeWebElementList[i].text)
-    singleRow = []
-    singleRow.append(articleTypeWebElementList[i].text)
-    singleRow.append(articleAndJournalNameWebElementList[2*i].text)
-    singleRow.append(articleAndJournalNameWebElementList[2*i + 1].text)
-    singleRow.append(articleInformationWebElementList[i].text)
-    singleRow.append(citationsCountWebElementList[i].text)
-    singleRow.append(authorsWebElementList[i].text)
-    dataRows.append(singleRow)
-    del singleRow
+for numberOfNext in range(2):       #provide here the number of next clicks avaiable
+    print("i = " + str(numberOfNext))
+    articleAndJournalNameWebElementList = driver.find_elements(By.XPATH, '//a[@title="Show document details"]') #Article and journal name
+    articleTypeWebElementList = driver.find_elements(By.XPATH,  '//div[@data-component="document-type"]') #article or + open, conference paper
+    
+    articleInformationWebElementList = driver.find_elements(By.XPATH,  '//span[@class="text-meta"]') #year, vol, issue, page nos, article no/
+    citationsCountWebElementList = driver.find_elements(By.XPATH,  '//div[@class="sc-els-info-field"]') #Citations count
+    
+    #players = driver.find_elements(By.XPATH,  '//div[@data-component="document-authors"]') #article or + open, conference paper
+    authorsWebElementList = driver.find_elements(By.XPATH,  '//div[@class="author-list"]') #Authors lists
+    for i in range(len(articleTypeWebElementList)):
+        print(articleTypeWebElementList[i].text)
+        singleRow = []
+        singleRow.append(articleTypeWebElementList[i].text)
+        singleRow.append(articleAndJournalNameWebElementList[2*i].text)
+        singleRow.append(articleAndJournalNameWebElementList[2*i + 1].text)
+        
+        #singleRow.append(articleInformationWebElementList[i].text)
+        articleInformation = articleInformationWebElementList[i].text
+        
+        #separatedInfo = articleInformation.split()        #splits the information into year, vol, issue, page_nos
+        # extract year, volume, issue, pages number
+        yearInfo = articleInformation[0:4]
+        if( len(articleInformation) > 4):
+            volumeIssuePageNosInfo = articleInformation[5:len(articleInformation)]
+        else:
+            volumeIssuePageNosInfo = ""
+        
+        singleRow.append(yearInfo)
+        singleRow.append(volumeIssuePageNosInfo)
+        singleRow.append(citationsCountWebElementList[i].text)
+        singleRow.append(authorsWebElementList[i].text)
+        dataRows.append(singleRow)
+        del singleRow
     #singleRow.clear()
+    a = driver.find_element_by_xpath("//span[contains(@class,'button__text sc-els-paginator') and contains(text(), 'Next')]").click()
+    time.sleep(5)
 #     
 # =============================================================================
 df = pd.DataFrame(dataRows)
+df.columns=["Article/Conference/Book Chapter", "Title", "Journal/Conference/Book chapter Name", "Year", "Vol/Issue/pages", "citations" , "Authors"]
 df.to_csv("final_report.csv")
+print(articleInformation)
 
 driver.quit()
--- untitled4.py
+++ untitled4.py
@@ -5,6 +5,7 @@ Created on Mon Oct 18 13:12:45 2021
 @author: bhupendra.singh
 """
 
+
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,11 +13,10 @@ import time  #replace with selenium wait
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-import pandas as pd
 
 
 os.chdir('C:/Users/bhupendra.singh/Documents/GitHub/fetchResearchersPublications')
-authorId = 57202256028
+authorId = 41461482100
 url = "https://www.scopus.com/authid/detail.uri?authorId=" + str(authorId)
 
 
@@ -28,40 +28,36 @@ driver = webdriver.Chrome(ChromeDriverManager().install() , options=options)
 driver.get(url)
 
 driver.refresh()
-time.sleep(15)
+time.sleep(10)
 print("title = " + driver.title)
 
-articleAndJournalNameWebElementList = driver.find_elements(By.XPATH, '//a[@title="Show document details"]') #Article and journal name
-articleTypeWebElementList = driver.find_elements(By.XPATH,  '//div[@data-component="document-type"]') #article or + open, conference paper
+count = 0
+a = driver.find_element_by_xpath("//span[contains(@class,'button__text sc-els-paginator') and contains(text(), 'Next')]")
+# Automating button click
+#driver.find_elements(By.XPATH,  '//button[@class="button--link-black sc-els-paginator"]') #Authors lists
+for i in range(5):
+    #driver.find_elements(By.XPATH, "//span[contains(@class,'button__text sc-els-paginator') and contains(text(), 'Next')]")
+    #driver.find_element_by_xpath("//span[contains(@class,'button__text sc-els-paginator') and contains(text(), 'Next')]").click();
+    print(i)
+    buttonStatus = a.
+    print("button status " + str(buttonStatus))
+    if(buttonStatus):
+        count = count + 1
+        print("Count = " + str(count))
+        a.click()
+        a = driver.find_element_by_xpath("//span[contains(@class,'button__text sc-els-paginator') and contains(text(), 'Next')]")
+    if(buttonStatus == False): 
+        driver.quit()
+        break
+    
+    #a = driver.find_element_by_xpath("//span[contains(@class,'button__text sc-els-paginator') and contains(text(), 'Next')]")
+    #print(a[0].text)
+    #print("cliekd = " + str(a.is_enabled()))
+    #a.click()
+    time.sleep(3)
+    
+print("Count = " + str(count))
 
-articleInformationWebElementList = driver.find_elements(By.XPATH,  '//span[@class="text-meta"]') #year, vol, issue, page nos, article no/
-citationsCountWebElementList = driver.find_elements(By.XPATH,  '//div[@class="sc-els-info-field"]') #Citations count
 
-#players = driver.find_elements(By.XPATH,  '//div[@data-component="document-authors"]') #article or + open, conference paper
-authorsWebElementList = driver.find_elements(By.XPATH,  '//div[@class="author-list"]') #Authors lists
-
-
-print(len(articleTypeWebElementList))
-
-
-dataRows = []
-singleRow = []
-
-for i in range(len(articleTypeWebElementList)):
-    print(articleTypeWebElementList[i].text)
-    singleRow = []
-    singleRow.append(articleTypeWebElementList[i].text)
-    singleRow.append(articleAndJournalNameWebElementList[2*i].text)
-    singleRow.append(articleAndJournalNameWebElementList[2*i + 1].text)
-    singleRow.append(articleInformationWebElementList[i].text)
-    singleRow.append(citationsCountWebElementList[i].text)
-    singleRow.append(authorsWebElementList[i].text)
-    dataRows.append(singleRow)
-    del singleRow
-    #singleRow.clear()
-#     
-# =============================================================================
-df = pd.DataFrame(dataRows)
-df.to_csv("final_report.csv")
 
 driver.quit()
